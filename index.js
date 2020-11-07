f06749a6501de7427bb5f68f2a616c9079981e66
litalz <litalbdbd@gmail.com>
2020-11-07 20:48:44 +0200

    pitures and colors

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ $("#about").click(function() {
         },
         'slow');
 });
+
+$(".blue").on("click", function() {
+    $('.picture').css('background-image', 'url(img/jcole/j.cole-blue.png');
+});
+
+$(".red").on("click", function() {
+    $('.picture').css('background-image', 'url(img/jcole/j.cole-red.png');
+});
+
+$(".green").on("click", function() {
+    $('.picture').css('background-image', 'url(img/jcole/j.cole-green.png');
+});
+
+$(".brown").on("click", function() {
+    $('.picture').css('background-image', 'url(img/jcole/j.cole-brown.png');
+});
